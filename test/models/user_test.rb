c53ab9bc60287d598e5c93538e08894e802bc253
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-14 12:14:43 +0800

    Write validation and tests for user name and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(name:"Dipyan", email:"dipyan@gmail.com")
+  end
+
+  test "User's name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "User email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end
