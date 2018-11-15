4580d422cdf41839a5f1e2ab3a89848832bbc163
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-15 11:01:31 +0800

    Write test and validation for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  validates :user_id, :post_id, :body, presence: true
   belongs_to :user
   belongs_to :post
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_110310) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "post_id"
+    t.integer "user_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.integer "comment_morph_id"
+    t.string "comment_morph_type"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -41,7 +40,4 @@ ActiveRecord::Schema.define(version: 2018_11_14_110310) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "users"
 end
