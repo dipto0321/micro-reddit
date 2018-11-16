9140a979b46b8c3c4fffb492b6232ca6909b7b60
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-16 17:03:05 +0800

    Add resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user
+
+  resources :post do
+    resources :comment
+  end
+
+  resources :comment do
+    resources :comment
+  end
 end
