b7fd76daa09348b1158a45223d9085409023e806
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-16 16:55:03 +0800

    Fix test fixture conflict

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :user_id, :post_id, :body, presence: true
+  validates :user_id, :comment_morph_type, :comment_morph_id, :body, presence: true
   belongs_to :user
   belongs_to :comment_morph, polymorphic: true
   has_many :comments, as: :comment_morph
